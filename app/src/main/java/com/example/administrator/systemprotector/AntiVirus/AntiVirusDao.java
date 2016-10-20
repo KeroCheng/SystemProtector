de478ddddfa315eefcba3671f8669af456b6051c
Leon.Cheng <892409025@qq.com>
2016-10-20 12:11:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/systemprotector/AntiVirus/AntiVirusDao.java
@@ -0,0 +1,21 @@
+package com.example.administrator.systemprotector.AntiVirus;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Administrator on 2016/10/18.
+ */
+public class AntiVirusDao {
+    public static String checkVirus(String md5) {
+        String desc = null;
+        SQLiteDatabase db=SQLiteDatabase.openDatabase("/data/data/com.example.administrator.systemprotector.AntiVirus/files/antivirus.db",null,SQLiteDatabase.OPEN_READONLY);
+        Cursor cursor=db.rawQuery("select desc from datable where md5=?",new String[]{md5});
+        if(cursor.moveToNext()){
+            desc=cursor.getString(0);
+        }
+        cursor.close();
+        db.close();
+        return desc;
+    }
+}
